e6093b0e598ce967c04431797557375f64ff7b5b
PeterLiuSHS <154510748+PeterLiuSHS@users.noreply.github.com>
2023-12-21 18:11:39 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//printf是库函数，专门用来打印数据，用之前打招呼。 #include<stdio.h>
+// main函数是程序的入口
+//运行：ctrl + F5
+#include<stdio.h>
+
+//int main()
+//{
+//		printf("小虎在行山，要注意安全\n");
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(long)); 
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 20;
+//	double jiage = 666.666;
+//	printf("%d\n", age);
+//	printf("%d\n", jiage);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;  //初始化
+//	//输入两个整数，计算求和，输出
+//	scanf("%d %d", &num1, &num2); //)sacnf是一个输入函数，&是取地址操作符
+//	int sum = num1 + num2;
+//	printf("%d\n", sum);
+//    return 0;
+//}
+
+
+int main()
+{
+	{
+		int a = 789767;
+		printf("%d\n", a);
+	}
+	return 0;
+}
+
